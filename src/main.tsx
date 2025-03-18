ef45d16d336267403ef9ae140be3d2a57f1969a5
Tai Vo <tantaivo74@gmail.com>
2025-03-18 21:12:50 +0700

    chore: install TanStack Query and Axios for API integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { render } from "preact";
 import "./index.css";
 import { App } from "./app.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-render(<App />, document.getElementById("app")!);
+const queryClient = new QueryClient();
+
+render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("app")!
+);
